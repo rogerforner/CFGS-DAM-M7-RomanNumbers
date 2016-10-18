cf03881f5e627a93829975d88f4b407cc0e21192
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-10-18 18:20:48 +0200

    provant opcions fins correcta

--- spec/RomanNumbersSpec.php
+++ spec/RomanNumbersSpec.php
@@ -12,4 +12,31 @@ class RomanNumbersSpec extends ObjectBehavior
     {
         $this->shouldHaveType(RomanNumbers::class);
     }
+
+    //per crear un altre test, fer un altra funció
+    function it_returns_I_if_1()
+    {
+       $this->convert(1)->shouldReturn('I');
+    }
+
+    function it_returns_II_if_2()
+    {
+        $this->convert(2)->shouldReturn('II');
+    }
+
+    function it_returns_III_if_3()
+    {
+        $this->convert(3)->shouldReturn('III');
+    }
+
+    function it_returns_V_if_5()
+    {
+        $this->convert(5)->shouldReturn('V');
+    }
+
+    function it_returns_X_if_10()
+    {
+        $this->convert(10)->shouldReturn('X');
+    }
+
 }
--- src/RomanNumbers.php
+++ src/RomanNumbers.php
@@ -2,4 +2,27 @@
 
 class RomanNumbers
 {
+    protected $code = [
+            1 => "I",
+            4 => "IV",
+            5 => "V",
+            9 => "IX",
+            10 => "X",
+        ];
+
+
+    public function convert($number){
+
+        $solution = "";
+
+        if($number == 4) {
+            $solution = $this->code[$number];
+            $number = 0;
+        }
+
+        $solution = $solution . str_repeat('I',$number);
+
+        return $solution;
+
+    }
 }
