b3fb8279c94d450280970b23efae4eb09067d09e
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-10-18 18:02:09 +0200

    phpspec

---
+++ spec/RomanNumbersSpec.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace spec;
+
+use RomanNumbers;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class RomanNumbersSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(RomanNumbers::class);
+    }
+}
