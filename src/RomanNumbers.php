40c35d0156058423303165405fc09821e4c9ee5f
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-10-25 16:04:52 +0200

    ok

--- src/RomanNumbers.php
+++ src/RomanNumbers.php
@@ -3,11 +3,19 @@
 class RomanNumbers
 {
     protected $code = [
-            1 => "I",
-            4 => "IV",
-            5 => "V",
-            9 => "IX",
+            1000 => "M",
+            900 => "CM",
+            500 => "D",
+            400 => "CD",
+            100 => "C",
+            90 => "XC",
+            50 => "L",
+            40 => "XL",
             10 => "X",
+            9 => "IX",
+            5 => "V",
+            4 => "IV",
+            1 => "I",
         ];
 
 
@@ -15,12 +23,12 @@ class RomanNumbers
 
         $solution = "";
 
-        if($number == 4) {
-            $solution = $this->code[$number];
-            $number = 0;
+        foreach ($this->code as $decimal => $roman){
+            if($number >= $decimal){
+                $solution .= $roman;
+                $number -= $decimal;
+            }
         }
-
-        $solution = $solution . str_repeat('I',$number);
 
         return $solution;
 
